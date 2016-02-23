65b73035400a784b3d0605b9c958b527c14de99f
Mateusz Dymczyk <dymczyk@gmail.com>
2016-02-23 16:50:15 +0900

    Adds tests and a bit of refactoring

--- lib/japanese/japanese_analyzer.rb
+++ lib/japanese/japanese_analyzer.rb
@@ -1,34 +1,35 @@
 # encoding: utf-8
 require 'natto'
 require 'load_jars'
+require 'set'
 
 # Analyzer of Japanese string data
 class JapaneseAnalyzer
 
-  def initialize(ignore_pos=%w(助詞 助動詞 記号 終助詞), options={})
-    @mecabs = {}
-    @mecab_options = options
-    # Pos to be skipped during parsing
-    @ignore_pos = ignore_pos
+  def initialize(ignore_pos=%w(助詞 助動詞 記号 終助詞), ignore_words=Set.new)
+    stop_pos = Java::scala.collection::Set.empty
+    ignore_pos.each do |pos|
+      stop_pos = stop_pos.+(Java::scala.collection.immutable::List.empty.send("::", pos))
+    end
+
+    stopwords = Java::scala.collection.immutable::Set.empty
+    ignore_words.each do |word|
+      stopwords = stop_pos.+(word)
+    end
+
+    @tokenizer = Java::com.enjapan.preprocessing.japanese.tokenizers.KuromojiTokenizer.new(
+        Java::com.atilika.kuromoji.ipadic::Tokenizer.new,
+        stopwords,
+        Java::scala.collection.immutable::Set.empty,
+        stop_pos)
   end
 
   def analyze(text)
-    tokenizer = Java::com.enjapan.preprocessing.japanese.tokenizers.KuromojiTokenizer.new(
-        Java::com.atilika.kuromoji.ipadic::Tokenizer.new,
-        Java::scala.collection.immutable::Set.empty,
-        Java::scala.collection.immutable::Set.empty,
-        Java::scala.collection::Set.empty.
-            +(Java::scala.collection.immutable::List.empty.send("::", "助詞")).
-            +(Java::scala.collection.immutable::List.empty.send("::", "助動詞")).
-            +(Java::scala.collection.immutable::List.empty.send("::", "記号")).
-            +(Java::scala.collection.immutable::List.empty.send("::", "終助詞"))
-    )
-
     result = {}
     i = 0
-    tokenized = Java::scala.collection.JavaConversions.seqAsJavaList(tokenizer.tokenizeWithMetadata(text)).to_a
+    tokenized = Java::scala.collection.JavaConversions.seqAsJavaList(@tokenizer.tokenizeWithMetadata(text)).to_a
     tokenized.each do |t|
-      surface = t.getSurface
+      surface = t.getBaseForm
       unless result.key?(surface)
         result[surface] = {
           pos: { :pos1 => t.getPartOfSpeechLevel1,
